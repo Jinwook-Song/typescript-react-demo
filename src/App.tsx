72680ef07ba710a12dcb10b6d5804e0d29693ced
jinwook-song <wlsdnr129@naver.com>
2021-04-17 13:36:46 +0900

    lecture 8.4 React State and Typescript

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,29 @@
-import React from "react";
+import React, { Component } from "react";
 
-function App() {
-  return <div></div>;
+interface IState {
+  counter: number;
+}
+
+class App extends Component<{}, IState> {
+  state = {
+    counter: 0,
+  };
+  render() {
+    const { counter } = this.state;
+    return (
+      <div>
+        {counter}
+        <button onClick={this.add}>Add</button>
+      </div>
+    );
+  }
+  add = () => {
+    this.setState((prev) => {
+      return {
+        counter: prev.counter + 1,
+      };
+    });
+  };
 }
 
 export default App;
